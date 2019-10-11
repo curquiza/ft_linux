51f8f82fc15cca97b3710a008708d5bebfc39503
Clementine Urquizar <clementine@meilisearch.com>
2019-10-11 17:04:50 +0200

    Add host checking and pre-installation;

---
+++ prepare-host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ln -sf /bin/bash /bin/sh
+apt install binutils \
+            coreutils \
+            build-essential \
+            bison \
+            gawk \
+            m4 \
+            texinfo
