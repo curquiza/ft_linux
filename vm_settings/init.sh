ad59b546ad924a6388630083b8b649ee030d060e
Clémentine Urquizar <clementine.urquizar@gmail.com>
2019-10-18 11:14:34 +0200

    Update init.sh

--- vm_settings/init.sh
+++ vm_settings/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y vim neovim curl git
+apt install -y vim neovim curl
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -9,6 +9,3 @@ mkdir -p $HOME/.config/nvim
 cp init.vim $HOME/.config/nvim/
 cp .gitconfig $HOME/
 cp .bashrc $HOME/
-
-# generate ssh key:
-# ssh-keygen -t rsa -b 4096 -C "clementine.urquizar@gmail.com"
